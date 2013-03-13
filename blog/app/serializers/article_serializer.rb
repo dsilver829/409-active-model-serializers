13b9dd232719715bbf74471a93f692e3e7eed1b9
David Silver <dsilver@candidatemetrics.com>
2013-03-13 05:47:02 -0700

    delegate current_user

--- blog/app/serializers/article_serializer.rb
+++ blog/app/serializers/article_serializer.rb
@@ -2,6 +2,7 @@ class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :name, :content
   has_many :comments
   embed :ids, include: true
+  delegate :current_user, to: :scope
 
   def url
     article_url(object)
@@ -9,7 +10,7 @@ class ArticleSerializer < ActiveModel::Serializer
 
   def attributes
     data = super
-    data[:edit] = edit_article_url(object) if scope.admin?
+    data[:edit] = edit_article_url(object) if current_user.admin?
     data
   end
 end
