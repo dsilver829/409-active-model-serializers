4274265ada8d00ae2559a1640b3717797baf17a9
David Silver <dsilver@candidatemetrics.com>
2013-03-13 05:44:20 -0700

    add edit_article_url

--- blog/app/serializers/article_serializer.rb
+++ blog/app/serializers/article_serializer.rb
@@ -1,3 +1,15 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :name, :content
+  has_many :comments
+  embed :ids, include: true
+
+  def url
+    article_url(object)
+  end
+
+  def attributes
+    data = super
+    data[:edit] = edit_article_url(object) if scope.admin?
+    data
+  end
 end
